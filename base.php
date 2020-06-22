99ce4eabf14f12694462a208e3200ff2004d8977
Zidan <b93605079@g.ntu.edu.tw>
2020-06-22 11:08:24 +0800

    建置base.php

--- base.php
+++ base.php
@@ -1,127 +1,170 @@
 <?php
 
 class DB{
-//設定屬性
-  private $dsn="mysql:host=localhost;charset=utf8;dbname=db99";
+  private $dsn="mysql:host=localhost;charset=utf8;dbname=db88";
   private $root="root";
   private $password="";
   private $table;
   private $pdo;
 
-
-//設定建構式，第13行的$table變數，因為第13行還沒執行，所以可以取一樣的變數名稱，但其實第13行和第8行是不同的。13行與14行的$table才是相同的。
   public function __construct($table){
     $this->table=$table;
     $this->pdo=new PDO($this->dsn,$this->root,$this->password);
-        
   }
 
-//取得全部資料
-  public function all(...$arg){
-    $sql="select * from $this->table";
-
-    if(!empty($arg[0]) && is_array($arg[0])) {
-      foreach($arg[0] as $key => $value){
-        $tmp[]=sprintf("`%s`='%s'",$key,$value);
-      }
-      $sql=$sql. " where " . implode(" && ",$tmp);
-    }
-
-    if(!empty($arg[1])){
-      $sql=$sql." ".$arg[1];
-    }
-    // echo $sql;
-    return $this->pdo->query($sql)->fetchAll();
-  }
-
-
-//取得單筆資料
-public function find($arg){
-  $sql="select * from $this->table";
-
-  if(is_array($arg)) {
-    foreach($arg as $key => $value){
-      $tmp[]=sprintf("`%s`='%s'",$key,$value);
-    }
-    $sql=$sql. " where " . implode(" && ",$tmp);
-  }else{
-    $sql=$sql. " where `id`='$arg'";
-  }
-
-  // echo $sql;
-  return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-}
-
-//計算筆數
-public function count(...$arg){
-  $sql="select count(*) from $this->table";
-
-  if(!empty($arg[0]) && is_array($arg[0])) {
-    foreach($arg[0] as $key => $value){
-      $tmp[]=sprintf("`%s`='%s'",$key,$value);
-    }
-    $sql=$sql. " where " . implode(" && ",$tmp);
-  }
-
-  if(!empty($arg[1])){
-    $sql=$sql." ".$arg[1];
-  }
-  // echo $sql;
-  return $this->pdo->query($sql)->fetchColumn();
-}
-
-
-//新增與更新資料
-  public function save($arg){
-    if(!empty($arg['id'])){
-      //更新
-      //$sql="update $this->table set xxx=yyy where `id` = 'xxx'";
-      foreach($arg as $key => $value){
-        if($key!='id'){
-        $tmp[]=sprintf("`%s`='%s'",$key,$value);
+    
+    public function all(...$arg){
+      $sql="select * from $this->table";
+    
+      if(!empty($arg[0]) && is_array($arg[0])) {
+        foreach($arg[0] as $key => $value){
+          $tmp[]=sprintf("`%s`='%s'",$key,$value);
         }
+        $sql=$sql. " where " . join(" && ",$tmp);
       }
-      $sql="update $this->table set ".implode(",",$tmp)." where `id`='".$arg['id']."'";
-    }else{
-      //新增
-      //$sql="insert into $this->table (``,``,``) values('','','');
-      $sql="insert into $this->table (`".implode("`,`",array_keys($arg))."`) values('".implode("','",$arg)."')";
+
+      if(!empty($arg[1])){
+        $sql=$sql." ".$arg[1];
+      }
+      // echo $sql;
+      return $this->pdo->query($sql)->fetchAll();
     }
-    return $this->pdo->exec($sql);
-  }
 
 
-//刪除資料
-public function del($arg){
-  $sql="delete from $this->table";
 
-  if(is_array($arg)) {
-    foreach($arg as $key => $value){
-      $tmp[]=sprintf("`%s`='%s'",$key,$value);
+
+    public function find($arg){
+      $sql="select * from $this->table";
+    
+      if(is_array($arg)) {
+        foreach($arg as $key => $value){
+          $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+        $sql=$sql. " where " . join(" && ",$tmp);
+      }else{
+        $sql=$sql. "where `id`='$arg'";
+      }
+      // echo $sql;
+      return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-    $sql=$sql. " where " . implode(" && ",$tmp);
-  }else{
-    $sql=$sql. " where `id`='$arg'";
-  }
 
-  // echo $sql;
-  return $this->pdo->exec($sql);
+
+
+
+    public function count(...$arg){
+      $sql="select count(*) from $this->table";
+    
+      if(!empty($arg[0]) && is_array($arg[0])){
+        foreach($arg[0] as $key => $value){
+          $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+        $sql=$sql. " where ". join(" && ",$tmp);
+      }
+      if(!empty($arg[1])){
+        $sql=$sql. " ". $arg[1];
+      }
+      // echo $sql;
+      return $this->pdo->query($sql)->fetchColumn();
+    }
+
+
+
+
+    public function save($arg){
+      if(!empty($arg['id'])){
+        //更新
+        foreach($arg as $key => $value){
+          if($key!='id'){
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+          }
+        }
+        $sql="update $this->table set ".join(",",$tmp)." where `id`='".$arg['id']."'";
+      }else{
+        //新增
+        $sql="insert into $this->table (`".join("`,`",array_keys($arg))."`) values('".join("','",$arg)."')";
+      }
+      // echo $sql;
+      return $this->pdo->exec($sql);
+    }
+
+
+
+
+    public function delete($arg){
+      $sql="delete from $this->table";
+    
+      if(is_array($arg)) {
+        foreach($arg as $key => $value){
+          $tmp[]=sprintf("`%s`='%s'",$key,$value);
+        }
+        $sql=$sql. " where " . join(" && ",$tmp);
+      }else{
+        $sql=$sql. "where `id`='$arg'";
+      }
+      // echo $sql;
+      return $this->pdo->exec($sql);
+    }
+
+
+
+    function q($sql){
+     return $this->pdo->query($sql)->fetchAll();
+    
+    }
+
 }
 
 
-//萬用語法
-function q($sql){
- return $this->pdo->query($sql)->fetchAll();
 
-}
-
-
-}
 
 
 function to ($url){
-  header("location:".$url);
+  header("location:" .$url);
 }
+
+
+
+
+// ★判斷瀏覽人次
+
+$total=new DB('total');
+$chk=$total->find(['date'=>date("Y-m-d")]);
+
+if(empty($chk) && empty($_SESSION['visited'])){
+  //今日第一位訪客
+  //頭香，需要新增今日資料。
+  $total->save(['date'=>date("Y-m-d"),"total"=>1]);
+  $_SESSOIN['visited']=1;
+
+}else if(empty($chk) && !empty($_SESSION['visited'])){
+  //沒有今天的資料，但有SESSION，1.手動更改日期，就是我們調整本機電腦的日期情況。2.23:59登入，過了一分鐘跨到隔天。
+  //異常情形，需要新增今日資料。
+  $total->save(['date'=>date("Y-m-d"),"total"=>1]);
+  
+}else if(!empty($chk) && empty($_SESSION['visited'])){
+  // 表示是新來的，需要+1
+  $chk['total']++;
+  $total->save($chk);
+  $_SESSOIN['visited']=1;
+
+}/*else{
+  // 有今天的資料，也有Session
+  
+}*/
+
+//最一開始測試用:
+// if(empty($_SESSION['visited'])){
+  
+//   if(empty($chk)){
+//     //沒有今天的資料
+//     $total->save(['date'=>date("Y-m-d"),'total'=>1]);
+//   }else{
+//     //有今天的資料
+//     $chk['total']++;
+//     $total->save($chk);
+//   }
+//   $_SESSION['visited']=1;
+// }
 
 
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,14 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<?php
+include_once "base.php";
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>健康促進網</title>
-<link href="./home_files/css.css" rel="stylesheet" type="text/css">
-<script src="./home_files/jquery-1.9.1.min.js"></script>
-<script src="./home_files/js.js"></script>
+<link href="./css/css.css" rel="stylesheet" type="text/css">
+<script src="./js/jquery-1.9.1.min.js"></script>
+<script src="./js/js.js"></script>
 </head>
 
 <body>
@@ -15,8 +18,21 @@
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
     	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
-        <div id="title2">
+
+				<?php
+					$sumVisited=$total->q("select sum(`total`) from `total`")[0][0];
+				?>
+
+				<?=date("m 月 d 日 l") ;?> | 今日瀏覽: 
+				<?php
+				$todayVisited=$total->find(['date'=>date("Y-m-d")]);
+				echo $todayVisited['total'];
+				?>
+				| 累積瀏覽: <?=$sumVisited;?>       
+				<a href="index.php" style="float:right";>回首頁</a>
+			</div>
+        <div id="title2" title="健康促進網-回首頁">
+					<a href="index.php"><img src="icon/02B01.jpg" alt="健康促進網-回首頁" title="健康促進網-回首頁"></a>
         	
         </div>
         <div id="mm">
@@ -41,7 +57,7 @@
         <div id="bottom">
     	    本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved 
     		 <br>
-    		 服務信箱：health@test.labor.gov.tw<img src="./home_files/02B02.jpg" width="45">
+    		 服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
     </div>
 
